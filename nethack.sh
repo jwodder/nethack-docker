4a3a600f61c15f7fd4ad91ee180da787cd0b9106
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-12-14 22:07:29 -0500

    Made a volumed version of the 3.6.0 image with a wrapper script for repopulating VARDIR

---
+++ nethack.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# This wrapper script ensures that /data is never empty when NetHack starts.
+# It omits the code in the official nethack.sh for setting X11 variables and
+# finding HACKPAGER, as those are irrelevant here and set by the Dockerfile,
+# respectively.
+
+HACKDIR=/usr/games/lib/nethackdir
+export HACKDIR
+HACK=$HACKDIR/nethack
+# NB: MAXNROFPLAYERS is deprecated in favor of MAXPLAYERS in SYSCF.
+MAXNROFPLAYERS=4
+VARDIR=/data
+
+# In case the user has mounted an empty directory into /data, ensure the
+# necessary files & directory exist so NetHack won't die:
+cd $VARDIR
+mkdir save
+touch perm record logfile xlogfile
+chown -R games .
+chgrp -R bin .
+chmod 0755 . save
+chmod 0644 perm record logfile xlogfile
+
+cd $HACKDIR
+case $1 in
+	-s*)
+		exec $HACK "$@"
+		;;
+	*)
+		exec $HACK "$@" $MAXNROFPLAYERS
+		;;
+esac
